da5724e36abe1ac08c4fd59f21da9a7cbeebd40d
rafitacorrea <rafitacorrea@yahoo.com.ar>
2020-01-23 17:40:23 -0300

    js terminado + version mobile + readme

---
+++ README.md
@@ -0,0 +1,3 @@
+Hello world!
+
+To run this code go to: http://..., look for the characters on the list and you'll find the movies they appear on!
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let names = [];
 let characters = [];
 let films = [];
 let filmsTitles = [];
+let filmsReduceFiltered = [];
+let filmsTitlesOk = [];
 const loadData = () => {
     axios.get('https://swapi.co/api/people/').then(response => {
         const people = response.data;
@@ -18,22 +20,37 @@ const loadData = () => {
 const createList = () => {
     names.sort();
     document.querySelector('.search-group-dropdown-content').innerHTML += names
-        .map((n, i) => `<li class="character"><input id="input${i}" type="checkbox" name="checkbox-character"><label for="input${i}">${n}</label></li>`)
+        .map(
+            (n, i) =>
+                `<li class="character" onclick="changeColor(${i})"><input class="input" id="input${i}" type="checkbox" name="checkbox-character"><label  for="input${i}">${n}</label></li>`
+        )
         .join('');
+};
+
+const changeColor = id => {
+    if (document.querySelector(`#input${id}`).checked) {
+        document.querySelector(`#input${id}`).parentNode.classList.add('character-selected');
+    } else {
+        document.querySelector(`#input${id}`).parentNode.classList.remove('character-selected');
+    }
 };
 
 const namesSelected = () => {
     document.querySelector('.movie-list-li').innerHTML = ``;
     characters = [];
+    films = [];
+    console.log(characters);
+    console.log(films);
     let checkboxes = document.getElementsByName('checkbox-character');
     checkboxes.forEach(c => {
         if (c.checked) {
             characters.push(c.parentNode.innerText);
         }
     });
-    console.log(characters);
-    createMovieList();
+    document.querySelector('.search-group-dropdown-content').innerHTML = '';
+    createList();
     searchMovie();
+    createMovieList();
 };
 
 const searchMovie = () => {
@@ -42,34 +59,40 @@ const searchMovie = () => {
             films.push(r.films);
         }
     });
-    let filmsReduce = films.flat().reduce((acum, f) => {
-        if (acum[f]) {
-            acum[f]++;
-        } else {
-            acum[f] = 1;
-        }
-        return acum;
-    }, {});
-    console.log(filmsReduce);
-    films.flat().forEach(f => {
-        if (filmsReduce[f] > 1) {
-            axios.get(f).then(response => {
-                document.querySelector('.movie-list-ul').innerHTML = `<ul class="movie-list-ul"></ul>`;
-                const titles = response.data;
-                filmsTitles = titles.title;
-                console.log(filmsTitles);
-                document.querySelector('.movie-list-li').innerHTML += `<li class="movie-list-li">${titles.title}</li>`;
-            });
-        }
-    });
+    films = films.flat();
+    if (characters.length > 1) {
+        let filmsReduce = films.reduce((acum, f) => {
+            if (acum[f]) {
+                acum[f]++;
+            } else {
+                acum[f] = 1;
+            }
+            return acum;
+        }, {});
+        films.forEach(f => {
+            if (filmsReduce[f] > 1) {
+                filmsReduceFiltered.push(f);
+                const uniqueSet = new Set(filmsReduceFiltered);
+                filmsTitlesOk = [...uniqueSet];
+            }
+        });
+    } else {
+        filmsTitlesOk = films;
+    }
 };
 
 const createMovieList = () => {
     document.querySelector('.movie-list-title').innerHTML = `<h3 class="movie-list-title">Movies</h3>
     <ul class="movie-list-ul">
     </ul>`;
+    filmsTitlesOk.forEach(ft => {
+        axios.get(ft).then(response => {
+            document.querySelector('.movie-list-ul').innerHTML = `<ul class="movie-list-ul"></ul>`;
+            const titles = response.data;
+            filmsTitles = titles.title;
+            document.querySelector('.movie-list-li').innerHTML += `<li class="movie-list-li">${titles.title}</li>`;
+        });
+    });
 };
 
 loadData();
-
-console.log(names);
